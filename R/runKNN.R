09514d27171cd13666f48b96c05cb0e498e11872
beyondpie <panshi.pie@gmail.com>
2022-01-27 19:03:39 -0800

    Major: add Annoy as another method for KNN.
    Annoy is fast but approximated.

--- R/runKNN.R
+++ R/runKNN.R
@@ -28,6 +28,8 @@ NULL
 #' computing the neighborhood overlap. Any edges with values less than or 
 #' equal to this will be set to 0 and removed from the SNN graph. Essentially 
 #' sets the strigency of pruning (0 --- no pruning, 1 --- prune everything).
+#' @param method character, "RANN" or "Annoy", the later is faster but approximated
+#' method.
 #' 
 #' @examples
 #' data(demo.sp);
@@ -42,7 +44,8 @@ NULL
 #' @return Returns the object with object@kmat filled
 #' @export
 
-runKNN <- function(obj, eigs.dims, weight.by.lambda, k, nn.eps, save.knn, filename, snn, snn.prune) {
+runKNN <- function(obj, eigs.dims, weight.by.lambda, k, nn.eps, save.knn,
+                   filename, snn, snn.prune, method) {
   UseMethod("runKNN", obj);
 }
 
@@ -56,7 +59,8 @@ runKNN.default <- function(
   save.knn = FALSE,
   filename = NULL,
   snn = FALSE,
-  snn.prune = 1/15
+  snn.prune = 1/15,
+  method = "RANN"
 ){
 	
 	cat("Epoch: checking input parameters\n", file = stderr())
@@ -119,15 +123,21 @@ runKNN.default <- function(
 	cat("Epoch: computing nearest neighbor graph\n", file = stderr())
 	
 	# exclude self neibours
+  if (method == "RANN") {
+    message("Using RANN to get the KNN result.")
     nn.ranked <- nn2(
-        data = data.use,
-        k = k,
-        searchtype = 'standard',
-        eps = nn.eps)$nn.idx;
+      data = data.use,
+      k = k,
+      searchtype = 'standard',
+      eps = nn.eps)$nn.idx
+  } else {
+    message("Using Annoy to get the KNN result.")
+    nn.ranked <- AnnoyNN(data = data.use, k = k)$nn.idx
+  }
 
 	j <- as.numeric(x = t(x = nn.ranked))
 	i <- ((1:length(x = j)) - 1) %/% k + 1	
-	edgeList = data.frame(i, j, 1);
+	edgeList <- data.frame(i, j, 1)
 		
 	if(snn){
 		cat("Epoch: converting knn graph into snn graph\n", file = stderr())	
@@ -155,5 +165,118 @@ runKNN.default <- function(
 
 
 
+####################
+## From Seurat
+####################
+# Run annoy
+#
+# @param data Data to build the index with
+# @param query A set of data to be queried against data
+# @param metric Distance metric; can be one of "euclidean", "cosine", "manhattan",
+# "hamming"
+# @param n.trees More trees gives higher precision when querying
+# @param k Number of neighbors
+# @param search.k During the query it will inspect up to search_k nodes which
+# gives you a run-time tradeoff between better accuracy and speed.
+# @param include.distance Include the corresponding distances
+# @param index optional index object, will be recomputed if not provided
+#
+AnnoyNN <- function(data,
+                    query = data,
+                    metric = "euclidean",
+                    n.trees = 50,
+                    k,
+                    search.k = -1,
+                    include.distance = TRUE,
+                    index = NULL
+) {
+  idx <- index %||% AnnoyBuildIndex(
+    data = data,
+    metric = metric,
+    n.trees = n.trees)
+  nn <- AnnoySearch(
+    index = idx,
+    query = query,
+    k = k,
+    search.k = search.k,
+    include.distance = include.distance)
+  nn$idx <- idx
+  nn$alg.info <- list(metric = metric, ndim = ncol(x = data))
+  return(nn)
+}
 
+# Build the annoy index
+#
+# @param data Data to build the index with
+# @param metric Distance metric; can be one of "euclidean", "cosine", "manhattan",
+# "hamming"
+# @param n.trees More trees gives higher precision when querying
+#
+#' @importFrom RcppAnnoy AnnoyEuclidean AnnoyAngular AnnoyManhattan AnnoyHamming
+#
+AnnoyBuildIndex <- function(data, metric = "euclidean", n.trees = 50) {
+  f <- ncol(x = data)
+  a <- switch(
+    EXPR = metric,
+    "euclidean" =  new(Class = RcppAnnoy::AnnoyEuclidean, f),
+    "cosine" = new(Class = RcppAnnoy::AnnoyAngular, f),
+    "manhattan" = new(Class = RcppAnnoy::AnnoyManhattan, f),
+    "hamming" = new(Class = RcppAnnoy::AnnoyHamming, f),
+    stop ("Invalid metric")
+  )
+  for (ii in seq(nrow(x = data))) {
+    a$addItem(ii - 1, data[ii, ])
+  }
+  a$build(n.trees)
+  return(a)
+}
 
+# Search an Annoy approximate nearest neighbor index
+#
+# @param Annoy index, built with AnnoyBuildIndex
+# @param query A set of data to be queried against the index
+# @param k Number of neighbors
+# @param search.k During the query it will inspect up to search_k nodes which
+# gives you a run-time tradeoff between better accuracy and speed.
+# @param include.distance Include the corresponding distances in the result
+#
+# @return A list with 'nn.idx' (for each element in 'query', the index of the
+# nearest k elements in the index) and 'nn.dists' (the distances of the nearest
+# k elements)
+#
+#' @importFrom future plan
+#' @importFrom future.apply future_lapply
+#
+AnnoySearch <- function(index, query, k, search.k = -1, include.distance = TRUE) {
+  n <- nrow(x = query)
+  idx <- matrix(nrow = n,  ncol = k)
+  dist <- matrix(nrow = n, ncol = k)
+  convert <- methods::is(index, "Rcpp_AnnoyAngular")
+  if (!inherits(x = plan(), what = "multicore")) {
+    oplan <- plan(strategy = "sequential")
+    on.exit(plan(oplan), add = TRUE)
+  }
+  res <- future_lapply(X = 1:n, FUN = function(x) {
+    res <- index$getNNsByVectorList(query[x, ], k, search.k, include.distance)
+    # Convert from Angular to Cosine distance
+    if (convert) {
+      res$dist <- 0.5 * (res$dist * res$dist)
+    }
+    list(res$item + 1, res$distance)
+  })
+  for (i in 1:n) {
+    idx[i, ] <- res[[i]][[1]]
+    if (include.distance) {
+      dist[i, ] <- res[[i]][[2]]
+    }
+  }
+  return(list(nn.idx = idx, nn.dists = dist))
+}
+
+`%||%` <- function(lhs, rhs) {
+  if (!is.null(x = lhs)) {
+    return(lhs)
+  } else {
+    return(rhs)
+  }
+}
